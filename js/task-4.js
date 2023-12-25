15d7414bb0c030d7003470f366de76873d5a2fb8
Spartak2 <orkotop030@gmail.com>
2023-12-25 03:45:58 +0200

    1

---
+++ js/task-4.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function getShippingCost(country) {
+let countryName;
+switch (country) {
+    case "China":
+        countryName = 'China';
+        return `Shipping to ${countryName} will cost 100 credits`;
+    case "Chile":
+        countryName = 'Chile';
+        return `Shipping to ${countryName} will cost 250 credits`
+    case "Australia":
+        countryName = 'Australia';
+        return `Shipping to ${countryName} will cost 170 credits`;
+    case "Jamaica":
+        countryName = 'Jamaica';
+        return `Shipping to ${countryName} will cost 120 credits`;
+    default:
+        return "Sorry, there is no delivery to your country";
+}
+}
+
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
