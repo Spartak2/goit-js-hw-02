74b04ff5490d3282483df37c78087de7bd165393
Spartak2 <orkotop030@gmail.com>
2023-12-26 01:10:01 +0200

    4

--- js/task-2.js
+++ js/task-2.js
@@ -2,9 +2,9 @@
 
 function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
-        return message;
+        return message.slice(0);
     } else {
-        return message.substring(0, maxLength - 3) + "...";
+        return message.substring(0, maxLength) + "...";
     }
 }
 
