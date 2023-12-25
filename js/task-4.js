840475a9988bc9d18b265ef954172dca1c0c6feb
Spartak2 <orkotop030@gmail.com>
2023-12-25 03:58:22 +0200

    2

--- js/task-4.js
+++ js/task-4.js
@@ -6,15 +6,19 @@ switch (country) {
     case "China":
         countryName = 'China';
         return `Shipping to ${countryName} will cost 100 credits`;
+        break;
     case "Chile":
         countryName = 'Chile';
         return `Shipping to ${countryName} will cost 250 credits`
+        break;
     case "Australia":
         countryName = 'Australia';
         return `Shipping to ${countryName} will cost 170 credits`;
+        break;
     case "Jamaica":
         countryName = 'Jamaica';
         return `Shipping to ${countryName} will cost 120 credits`;
+        break;
     default:
         return "Sorry, there is no delivery to your country";
 }
